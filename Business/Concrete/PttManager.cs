6816b61dc4ee01cacb806d937e4e1b55020c62f2
Hüseyin Özükcan <huseyinokeanusozukcan01@gmail.com>
2024-02-06 13:43:56 +0300

    Proje dosyası ekle.

---
+++ Business/Concrete/PttManager.cs
@@ -0,0 +1,31 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete;
+
+public class PttManager : ISupplierService
+{
+    //Dependency Injection
+    private IApplicantService _applicantService;
+    public PttManager(IApplicantService applicantService)
+    {
+         _applicantService = applicantService;
+    }
+    public void GiveMask(Person person)
+    {
+        if (_applicantService.CheckPerson(person))
+        {
+            Console.WriteLine(person.FirstName + " için maske verildi");
+        }
+        else
+        {
+            Console.WriteLine(person.FirstName + " için maske VERİLEMEDİ");
+        }
+    }
+}
---
+++ WorkSpace/Program.cs
@@ -0,0 +1,24 @@
+﻿//Degiskenler();
+
+using Business.Concrete;
+using Entities.Concrete;
+
+Person person1 = new Person();
+person1.FirstName = "Engin";
+person1.LastName = "Demiroğ";
+person1.DateOfBirthYear = 1985;
+person1.NationalIdentity = 123;
+
+PttManager pttManager = new PttManager(new PersonManager());
+pttManager.GiveMask(person1);
+
+
+static void Degiskenler()
+{
+    string mesaj = "Merhaba";
+    double tutar = 100000; //db'den gelir
+    int sayi = 100;
+    bool girisYapmisMi = false;
+    Console.WriteLine(tutar * 1.18);
+    Console.WriteLine(tutar * 1.18);
+}
